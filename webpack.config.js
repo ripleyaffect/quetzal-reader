83d566469c72bb7e8be66600303140b34d8a9654
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-06-05 18:49:08 -0400

    Add optional plugin for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+let plugins = []
+
+
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = plugins.concat(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  )
+}
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -18,5 +33,6 @@ module.exports = {
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.es6']
-  }
+  },
+  plugins: plugins,
 }
