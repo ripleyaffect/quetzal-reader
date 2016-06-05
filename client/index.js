3f87ae57dafec52cdc43eca0f2cdd7713add8404
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-06-05 18:11:43 -0400

    Add basic react mount

--- client/index.js
+++ client/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world from the command line')
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => <h1>Hello world from React</h1>
+
+render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,19 @@ module.exports = {
     path: __dirname + '/public',
     filename: 'bundle.js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.(es6|jsx?)$/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        include: __dirname + '/client'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.es6']
+  }
 }
