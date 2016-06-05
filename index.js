cf117c36b7dcd1a352ba96137abe9129ce245dfb
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-06-05 16:29:32 -0400

    Add dirname to path

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ var app = express()
 app.use('/static', express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html')
+  try {
+    res.sendFile(__dirname + '/public/index.html')
+  } catch(err) {
+    res.send(err)
+  }
 })
 
 app.listen(PORT, function() {
