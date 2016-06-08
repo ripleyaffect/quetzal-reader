00948b30d9510587aca32ab07c33a661dcc8ed9b
Thomas Peterson <t.ripley.peterson@gmail.com>
2016-06-07 21:06:26 -0400

    add prev/next card ids

--- client/components/CardStack.jsx
+++ client/components/CardStack.jsx
@@ -1,21 +1,33 @@
+import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
 
 import Card from './Card'
 
-const CardStack = ({ title }) => {
+const CardStack = ({ nextCardId, prevCardId, title }) => {
   return <div className="card-stack">
     <h1>{title}</h1>
     <Card />
     <div className="card-stack-navigation">
-      <div>&lt; back</div>
-      <div>forward &gt;</div>
+      {prevCardId && <div>&lt; back to {prevCardId}</div>}
+      {nextCardId && <div>forward to {nextCardId} &gt;</div>}
     </div>
   </div>
 }
 
 const mapStateToProps = (state) => {
-  return state.cardStacksById[state.activeCardStackId]
+  const activeStack = state.cardStacksById[state.activeCardStackId]
+  console.log(activeStack)
+
+  const activeCardIndex = activeStack.cards.indexOf(state.activeCardId)
+  console.log(activeCardIndex)
+  return Object.assign({},
+    activeStack,
+    {
+      prevCardId: activeStack.cards[activeCardIndex - 1],
+      nextCardId: activeStack.cards[activeCardIndex + 1],
+    }
+  )
 }
 
 export default connect(mapStateToProps)(CardStack)
